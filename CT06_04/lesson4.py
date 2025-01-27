4d62d398e12718f5d9e019340513cbb6fc2ba158
GwenTan83 <Gwenyining@gmail.com>
2025-01-27 09:55:32 +0000

    Jan 27, 2025, 9:55 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -3,7 +3,7 @@ red=3*1
 blue=2*5
 green=3*4
 print("red+green+blue")
-reply=input("What is your name")
+reply=input("What is your name?")
 print("Hello"+reply)
 reply=input("What is your name?")
 print(reply)
