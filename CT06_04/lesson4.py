94209e93c51e1c5de0ed21c3999e66faf7ebb4d8
GwenTan83 <Gwenyining@gmail.com>
2025-01-27 10:14:59 +0000

    Jan 27, 2025, 10:14 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -2,15 +2,15 @@ print("Hello from lesson 4")
 red=3*1
 blue=2*5
 green=3*4
-print("red+green+blue")
-reply=input("What is your name?")
-print("Hello"+reply)
-reply=input("What is your favourite colour?")
-print(reply)
-reply=input("What is your age?")
-print(reply)
-counter=500
-print(counter*reply)
+print(red+green+blue)
+# reply=input("What is your name?")
+# print("Hello"+reply)
+# reply=input("What is your favourite colour?")
+# print(reply)
+# reply=input("What is your age?")
+# print(reply)
+# counter=500
+# print(counter*reply)
 
 name=input("What is your name?")
 print("Hey,"+name)
