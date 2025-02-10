0d08f98391c866b3c4e406c962d3ee210c1c62ca
GwenTan83 <Gwenyining@gmail.com>
2025-02-10 10:16:23 +0000

    Feb 10, 2025, 10:16 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -73,4 +73,5 @@ for number in range(3,0,-1):
 # Create a countdown timer that counts from 10 to 1.
 # When the timer hits 1, print "Boo!"
 for number in range(10,0,-1):
-    print
+    print(number)
+print("")
