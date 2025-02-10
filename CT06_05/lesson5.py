3573ed935857586e1696d066599d34551a0d13e1
GwenTan83 <Gwenyining@gmail.com>
2025-02-10 10:11:40 +0000

    Feb 10, 2025, 10:11 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -43,12 +43,13 @@ for number in range(18,30):
 
 # **Task 6a**:
 # Use a 'for' loop to print numbers from 2 to 24 in multiples of 2.
-for number in range(2,25,2):
-     print (number)
+# for number in range(2,25,2):
+#      print (number)
 
 # **Task 6b**:
 # Use a 'for' loop to print numbers from 8 to 96 in multiples of 8.
-for number in range (8,97,8):
-     print(number)
+# for number in range (8,97,8):
+#      print(number)
 # **Task 6c**:
 # Use a 'for' loop to print numbers from 5 to 1 in descending order.
+for number in range (5,0):
