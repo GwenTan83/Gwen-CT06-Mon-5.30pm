61833fe4cd33bad611b20f1bcc862c0dfa90491a
GwenTan83 <Gwenyining@gmail.com>
2025-02-17 10:33:54 +0000

    Feb 17, 2025, 10:33 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -8,6 +8,9 @@
 
 # print(sumScore/numStud)
 
-for i in range(3):
-    print("Hello, World!")
+# **Task 1a**:
+# for i in range(3):
+#     print("Hello, World!")
+# **Task 1b**:
+for i in range(5):(input(i))
 
