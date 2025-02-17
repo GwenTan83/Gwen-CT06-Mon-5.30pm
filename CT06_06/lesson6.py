d33b8a58eb7e9d8df8486bef0e15726471370e77
GwenTan83 <Gwenyining@gmail.com>
2025-02-17 10:11:14 +0000

    Feb 17, 2025, 10:11 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,8 +1,8 @@
 print("Hello from lesson 6")
-sumscore=0
+sumScore=0
 numstudents=int(input("How many students do you have?"))
 for i in range (numstudents):
     score=int(input("How much did this student score?"))
-    
+    sumScore=
 
 
