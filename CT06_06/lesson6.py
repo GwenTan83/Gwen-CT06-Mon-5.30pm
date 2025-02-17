0de5a71261f04e85f146f4b4a23945d3f1bfe640
GwenTan83 <Gwenyining@gmail.com>
2025-02-17 10:44:18 +0000

    Feb 17, 2025, 10:44 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -12,7 +12,14 @@
 # for i in range(3):
 #     print("Hello, World!")
 # **Task 1b**:
-for i in range(5):
-    (input(i))
+# for i in range(5):
+#     print(i)
+# **Task 1c**:
+# print("Hello, World!")
+# **Task 1d**:
+# ifs = 5
+
+# **Task 1e**:
+# print ("Hello, World!")
 
 
