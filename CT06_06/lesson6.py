5a791935bcc580fab3e3f6df6d6ac325c497d900
GwenTan83 <Gwenyining@gmail.com>
2025-02-17 10:30:42 +0000

    Feb 17, 2025, 10:30 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,12 +1,12 @@
 # print("Hello from lesson 6")
 
-sumScore=0
-numStud=int(input("How many students do you have?"))
-for i in range(numStud):
-    score=int(input("How much did this student score?"))
-    sumScore=sumScore + score
+# sumScore=0
+# numStud=int(input("How many students do you have?"))
+# for i in range(numStud):
+#     score=int(input("How much did this student score?"))
+#     sumScore=sumScore + score
 
-print(sumScore/numStud)
+# print(sumScore/numStud)
 
 for i in range(3):
     print("Hello, World!")
