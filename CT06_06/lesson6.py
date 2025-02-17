f8137a835aca99e5c1cb2010f92f7aa1047997ef
GwenTan83 <Gwenyining@gmail.com>
2025-02-17 10:14:34 +0000

    Feb 17, 2025, 10:14 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -2,7 +2,7 @@
 
 sumScore=0
 numStud=int(input("How many students do you have?"))
-for i in range(numstud):
+for i in range(numStud):
     score=int(input("How much did this student score?"))
     sumScore=sumScore + score
 
