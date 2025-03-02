0859fc1be279310ac6c532bec8d68cef2629e413
GwenTan83 <Gwenyining@gmail.com>
2025-03-02 09:06:54 +0000

    Mar 2, 2025, 9:06 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -7,19 +7,20 @@ print("Hello from lesson 8")
 # 1. Using a for loop, ask the user for 5 numbers one at a time.
 # 2. Calculate the multiplication for these 5 numbers and print
 #    it out.
-ans = 1
-for (i) in range(1, 6):
-    userResponse= int(input("What is number #" + str(1) + "? "))
-    ans= ans * userResponse
-print("The product of the 5 numbers is" + str(ans))
+# ans = 1
+# for (i) in range(1, 6):
+#     userResponse= int(input("What is number #" + str(1) + "? "))
+#     ans= ans * userResponse
+# print("The product of the 5 numbers is" + str(ans))
 
-import time
+# import time
 
-start = int(input("What number do you want to start from?"))
+# start = int(input("What number do you want to start from?"))
 
-for i in range(start, 0, -1):
-    print(i)
-    time.sleep(1)
+# for i in range(start, 0, -1):
+#     print(i)
+#     time.sleep(1)
 
-print("Liftoff!")
+# print("Liftoff!")
+
 
