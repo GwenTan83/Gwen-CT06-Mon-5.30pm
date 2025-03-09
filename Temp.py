7e254c9ae8ea3cfc54d494a4f7b10a8fb5ed8470
GwenTan83 <Gwenyining@gmail.com>
2025-03-09 08:45:15 +0000

    Mar 9, 2025, 8:45 AM

--- Temp.py
+++ Temp.py
@@ -3,8 +3,8 @@
 
 for number in range(10,0,-1):
     print(number)
-start = int(input("Enter number: 4"))
-end = int(input("Enter number: 10"))
-increment = int(input("Enter number: 2"))
+start = int(input("4"))
+end = int(input("10"))
+increment = int(input("2"))
 for i in range (start, end , increment):
     print(i)
