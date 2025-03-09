4384a860e38cadb4901b90e69192e8fbd0c1e570
GwenTan83 <Gwenyining@gmail.com>
2025-03-09 09:02:57 +0000

    Mar 9, 2025, 9:02 AM

--- Temp.py
+++ Temp.py
@@ -21,4 +21,4 @@ for i in range (start,end,increment):
 Start = int(input("What is the first number?"))
 End = int(input("What is the second number?"))
 Increment = int(input("What is the third number?"))
-print(Start, End)
+print(Start, End, Increment)
