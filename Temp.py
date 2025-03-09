6f089c4c7873b56635f2ec700cdab2cda69a5308
GwenTan83 <Gwenyining@gmail.com>
2025-03-09 08:46:55 +0000

    Mar 9, 2025, 8:46 AM

--- Temp.py
+++ Temp.py
@@ -1,8 +1,7 @@
 # name = input("What is your name?")
 # print("Nice to meet you," + name + "!")
 
-for number in range(10,0,-1):
-    print(number)
+
 start = int(input("4"))
 end = int(input("10"))
 increment = int(input("2"))
