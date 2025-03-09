02fc6fb164bdeecaa0ed540993be20a291dcfc21
GwenTan83 <Gwenyining@gmail.com>
2025-03-09 08:53:45 +0000

    Mar 9, 2025, 8:53 AM

--- Temp.py
+++ Temp.py
@@ -13,3 +13,8 @@ for i in range (start,end,increment):
 # increment = 7
 # for i in range(0,77,7):
 #     print(output)
+start = 0
+end = int(input("77"))
+increment = int(input("7"))
+for i in range (start,end,increment):
+    print(i)
