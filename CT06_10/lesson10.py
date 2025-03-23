56412737f0e8ac0263f4ca2c02df6dc988eeec8b
GwenTan83 <Gwenyining@gmail.com>
2025-03-23 08:50:26 +0000

    Mar 23, 2025, 8:50 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,9 +1,9 @@
 # print("Hello from lesson 10")
-num = input(int("What number do you want to check?"))
-if num>0:
-    print(str(num) + "number is positive")
-else:
-    print(str(num) + "number is negative")
+# num = input(int("What number do you want to check?"))
+# if num>0:
+#     print(str(num) + "number is positive")
+# else:
+#     print(str(num) + "number is negative")
 
 num = input(int("What number do you want to check?"))
 ans = num % 2
