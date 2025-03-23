5507ac4515da2ddd2836cabde541e43d638449e7
GwenTan83 <Gwenyining@gmail.com>
2025-03-23 08:50:03 +0000

    Mar 23, 2025, 8:50 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,9 +1,13 @@
 # print("Hello from lesson 10")
 num = input(int("What number do you want to check?"))
 if num>0:
-    print(str(num)+ "number is positive")
+    print(str(num) + "number is positive")
 else:
-    print(str(num)+ "number is negative")
+    print(str(num) + "number is negative")
 
 num = input(int("What number do you want to check?"))
 ans = num % 2
+if ans==0:
+    print(str(num) + "This number is even.")
+else:
+    print(str(num) + "This number is odd.")
