2b0f5ed8c05f1cd88ad5732033481849049198df
GwenTan83 <Gwenyining@gmail.com>
2025-03-30 08:35:09 +0000

    Mar 30, 2025, 8:35 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,5 +1,5 @@
 # print("Hello from lesson 11")
-number=input(int("What number do you want to check?"))
+number=int(input("What number do you want to check?"))
 if number % 3 ==0 and number % 7 ==0:
     print("The number is divisible by 3 and 7!")
 else:
