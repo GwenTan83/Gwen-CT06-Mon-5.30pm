d0be43a560bedc2bf0d5f33f5d5564a332af9999
GwenTan83 <Gwenyining@gmail.com>
2025-03-30 08:43:32 +0000

    Mar 30, 2025, 8:43 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,9 +1,9 @@
 # print("Hello from lesson 11")
-number=int(input("What number do you want to check?"))
-if number % 3 ==0 and number % 7 ==0:
-    print("The number is divisible by 3 and 7!")
-else:
-    print("The number is not divisible by 3 and 7!")
+# number=int(input("What number do you want to check?"))
+# if number % 3 ==0 and number % 7 ==0:
+#     print("The number is divisible by 3 and 7!")
+# else:
+#     print("The number is not divisible by 3 and 7!")
 
 age=int(input("What is your age?"))
 if age<12 or age>65:
