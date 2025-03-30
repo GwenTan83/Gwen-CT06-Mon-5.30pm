d7f4a5a02eec5ffb8fb53e403f0f9241f6a3f1ff
GwenTan83 <Gwenyining@gmail.com>
2025-03-30 08:20:44 +0000

    Mar 30, 2025, 8:20 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -12,11 +12,24 @@
 # else:
 #     print("This number is odd.")
 
-age = int(input("What is your age?"))
-if age<13:
-    print("You are a child.")
-else:
-    if age<19:
-        print("You are a teen.")
-    else:
-        print("You are an adult.")
+# age = int(input("What is your age?"))
+# if age<13:
+#     print("You are a child.")
+# else:
+#     if age<19:
+#         print("You are a teen.")
+#     else:
+#         print("You are an adult.")
+
+score = int(input("What is your score?"))
+
+if score < 60:
+    print("F")
+elif score <= 69:
+    print("D")
+elif score <= 79:
+    print("C")
+elif score <= 89:
+    print("B")
+elif score <= 100:
+    print("A")
