531c5afd0b70576888df73265765b157200a23b2
GwenTan83 <Gwenyining@gmail.com>
2025-04-13 09:04:20 +0000

    Apr 13, 2025, 9:04 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,15 +1,16 @@
 # print("Hello from lesson 13")
-# groceries=[
-# "Apples",
-# "Bread",
-# "Carrots",
-# "Dates",
-# "Eggs",
-# "Flour",
-# "Grapes",
-# "Honey"]
-# groceries[7]="Herbs"
-# print (groceries)
-# groceries.append("Ice")
-# groceries.insert(1,"Bananas")
-# print(groceries)
+groceries=[
+"Apples",
+"Bread",
+"Carrots",
+"Dates",
+"Eggs",
+"Flour",
+"Grapes",
+"Honey"]
+groceries[7]="Herbs"
+print (groceries)
+groceries.append("Ice")
+groceries.insert(1,"Bananas")
+print(groceries)
+del()
