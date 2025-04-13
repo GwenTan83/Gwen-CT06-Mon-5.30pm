17128f6f541fd4a2d5c576b67790f96c68016dac
GwenTan83 <Gwenyining@gmail.com>
2025-04-13 08:56:17 +0000

    Apr 13, 2025, 8:56 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,12 +1,13 @@
 # print("Hello from lesson 13")
-# groceries=[
-# "Apples",
-# "Bread",
-# "Carrots",
-# "Dates",
-# "Eggs",
-# "Flour",
-# "Grapes",
-# "Honey"]
-# groceries[7]="Herbs"
-# print (groceries)
+groceries=[
+"Apples",
+"Bread",
+"Carrots",
+"Dates",
+"Eggs",
+"Flour",
+"Grapes",
+"Honey"]
+groceries[7]="Herbs"
+print (groceries)
+
