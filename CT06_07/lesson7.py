969a95ceddcc4cedb67a7f436ddb87322812084b
GwenTan83 <Gwenyining@gmail.com>
2025-04-13 09:09:02 +0000

    Apr 13, 2025, 9:09 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -17,3 +17,11 @@ end = int(input("Enter number: 10"))
 increment = int(input("Enter number: 2"))
 for i in range (start, end , increment):
     print(i)
+
+# Task 2: List of groceries (part 2)
+# 1. Use a 'for' loop and print out all the groceries on your list
+# 2. If grocery == "Apples", print "<grocery name>: I need 5 of these"
+# 3. If grocery == "Carrots", print "<grocery name>: I need 3 of
+#    these"
+# 4. If name == "Grapes", print "<grocery name>: Get the FarmFresh
+#    brand"
