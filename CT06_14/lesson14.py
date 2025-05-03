f94bbba99cb584180eeafc6a99bac8c132b87b4d
GwenTan83 <Gwenyining@gmail.com>
2025-05-03 16:49:02 +0000

    May 3, 2025, 4:49 PM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -70,7 +70,7 @@ window = turtle.Screen()
 window.setup(width=600, height=400)
 t = turtle.Turtle()
 t.shape("turtle")
-t.fillcolor("purple")
+t.fillcolor("green")
 window.mainloop()
 
 # TEST REVISION
