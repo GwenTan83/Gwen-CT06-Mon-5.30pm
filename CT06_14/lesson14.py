2fc827becadb4790abb95ba8a51609641bf25765
GwenTan83 <Gwenyining@gmail.com>
2025-05-03 17:38:40 +0000

    May 3, 2025, 5:38 PM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -113,7 +113,7 @@ import turtle
 window=turtle.Screen()
 window.setup(width=600,  height=400)
 t=turtle.Turtle()
-
+t.shape("turtle")
 # ---------------------------------------------------------------------------------------------------
 # TEST REVISION
 
