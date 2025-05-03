3fab2c91c4cb01acc527d2647396361aac8ae4d7
GwenTan83 <Gwenyining@gmail.com>
2025-05-03 17:24:42 +0000

    May 3, 2025, 5:24 PM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -82,9 +82,9 @@ t.fillcolor("green")
 t.seth(0)
 t.pendown()
 
-for i in range(3):
-    t.forward(1)
-    t.left(360/3)
+for i in range(4):
+    t.forward(100)
+    t.left(90)
 
 window.mainloop()
 
