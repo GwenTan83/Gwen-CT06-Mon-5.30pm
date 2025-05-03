dae027018dce21107e0924dbc0f55ee1bc87f73c
GwenTan83 <Gwenyining@gmail.com>
2025-05-03 16:53:19 +0000

    May 3, 2025, 4:53 PM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -79,13 +79,13 @@ for i in range(3):
 
 window.mainloop()
 
-**Task 3b**: Draw a triangle
-Number of sides: 3
-Interior angle: 120
+# **Task 3b**: Draw a triangle
+# Number of sides: 3
+# Interior angle: 120
 
-**Task 3c**: Draw a square
-Number of sides: 4
-Interior angle: 90
+# **Task 3c**: Draw a square
+# Number of sides: 4
+# Interior angle: 90
 
 # TEST REVISION
 
