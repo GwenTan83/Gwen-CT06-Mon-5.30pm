144bc2fd3e49d083a757f85adf43250ab0153ca9
GwenTan83 <Gwenyining@gmail.com>
2025-05-03 17:38:51 +0000

    May 3, 2025, 5:38 PM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -114,6 +114,7 @@ window=turtle.Screen()
 window.setup(width=600,  height=400)
 t=turtle.Turtle()
 t.shape("turtle")
+t.fillcdolor("green")
 # ---------------------------------------------------------------------------------------------------
 # TEST REVISION
 
