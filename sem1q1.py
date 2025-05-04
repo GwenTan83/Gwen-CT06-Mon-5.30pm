6500d49646fc593d01470577cad8b7cce45aa8ac
GwenTan83 <Gwenyining@gmail.com>
2025-05-04 08:38:47 +0000

    May 4, 2025, 8:38 AM

--- sem1q1.py
+++ sem1q1.py
@@ -36,7 +36,7 @@ while True:
 planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 print(planets[2])
 
-# groceries.append("Ice")
+planets.append("Ice")
 
 # 1. Write code below to print the 3rd item 
 #    in this list using index e.g. earth
