b0fba972405d2c8ded03497c41135c4a1e798d38
GwenTan83 <Gwenyining@gmail.com>
2025-05-04 09:02:19 +0000

    May 4, 2025, 9:02 AM

--- sem1q1.py
+++ sem1q1.py
@@ -1,4 +1,11 @@
-# task 1 question 1
+# Task 1
+# Print numbers from 10 to 200 using the while loop
+# Your numbers must be in multiples of 10.
+# 10 must be first number printed, and 200 the last number.
+
+# Example: 10, 20, 30 ..... 180, 190, 200.
+# Note that the numbers do not need to be printed in one line.
+# Write your code here
 number=0
 while True:
     number=number+10
@@ -6,16 +13,24 @@ while True:
     if number==200:
         break
 
+# Task 2
+# Code a password checker to protect your code!
 
-# task 2 question 1
-Pw=input("What is your password?")
-if Pw=="superpass123":
-    print("Access granted!")
-else:
-    print("Access denied!")
+# Assign a password "superpass123" to a variable
+# Ask the user to enter a password
+# If the password matches, print “Access Granted”
+# If the password does not match, print “Access Denied”
+
+# Write your code here
+
+# Pw=input("What is your password?")
+# if Pw=="superpass123":
+#     print("Access granted!")
+# else:
+#     print("Access denied!")
 
 
-# task 2 question 2
+
 planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 print(planets[2])
 
