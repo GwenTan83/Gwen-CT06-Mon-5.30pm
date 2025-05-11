66352f7c3599447fae20ba82e3eeabe8d1f644fe
GwenTan83 <Gwenyining@gmail.com>
2025-05-11 08:21:08 +0000

    May 11, 2025, 8:21 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -5,7 +5,7 @@
 # Using the 'turtle' library, create a 200x200 window
 import turtle
 window=turtle.Screen()
-
+window.setup(200,200)
 # 1. Import 'turtle' library
 # 2. Using '.setup()', create a window 200 in width and 200 in height
 # 3. Add a '.mainloop()' function to keep the window open
