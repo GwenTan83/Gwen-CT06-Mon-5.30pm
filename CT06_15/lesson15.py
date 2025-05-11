626e79315257a0bc4daf33ede83fde8bb23619c6
GwenTan83 <Gwenyining@gmail.com>
2025-05-11 08:51:51 +0000

    May 11, 2025, 8:51 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -75,7 +75,7 @@
 
 person=input("What is your name?")
 def functionName(greet):
-    greet="Hi there!"
+    greet="Hi there!" "My name is Freddo"
     if person=="Ethan":
         print("Hi Ethan. How are your?")
     elif person=="Ben" or person=="Gracie" or person=="Javior":
