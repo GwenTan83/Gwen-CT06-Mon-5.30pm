a6fd5c91d9eda792e7465b3ec4ca9013e0f6866d
GwenTan83 <Gwenyining@gmail.com>
2025-05-18 08:19:36 +0000

    May 18, 2025, 8:19 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -118,3 +118,12 @@ for _ in range(3):
             draw_square()
             t.forward(20)
     t.backward(num_squares * 20)
+
+# Task 6: Greetings III
+# Create a function that takes in a name and returns a greeting
+
+# 1. Create a function named 'greet()'
+# 2. The function should return a greeting
+#     Example: "Hello there !"
+# 3. Ask the user for their name
+# 4. Using the 'greet()' function, print the greeting
