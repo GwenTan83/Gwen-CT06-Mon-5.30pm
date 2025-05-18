2e7c886a7ba4d477662a60208061c77ed224670f
GwenTan83 <Gwenyining@gmail.com>
2025-05-18 08:18:39 +0000

    May 18, 2025, 8:18 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -1,1 +1,10 @@
 print("Hello from lesson 16")
+# Task 6: Greetings III
+# Create a function that takes in a name and returns a greeting
+
+# 1. Create a function named 'greet()'
+# 2. The function should return a greeting
+#     Example: "Hello there !"
+# 3. Ask the user for their name
+# 4. Using the 'greet()' function, print the greeting
+
