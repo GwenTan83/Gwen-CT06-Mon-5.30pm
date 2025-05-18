454f95fcb1a4fa67a7a6be44e554a8061ce6c531
GwenTan83 <Gwenyining@gmail.com>
2025-05-18 08:49:17 +0000

    May 18, 2025, 8:49 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -13,7 +13,7 @@ screen=setup_screen(screenWidth,screenHeight)
 # Keeps window open
 screen.mainloop()
 
-# Existing code to create
+# Existing code to create setup_screen function
 def create_blue_ball():
     ball=turtle.Turtle()
     ball.shape("circle")
