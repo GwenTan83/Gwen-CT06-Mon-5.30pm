33e1b7628d3e71aa811422224c6cc3c1ef7e79a9
GwenTan83 <Gwenyining@gmail.com>
2025-05-18 08:39:44 +0000

    May 18, 2025, 8:39 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -1,12 +1,12 @@
 print("Hello from lesson 16")
 
 import turtle
-# returns a window that is screenWidth
+# returns a window that is screenWidth x screenHeight
 def setup_screen(screenWidth,screenHeight):
     screen=turtle.Screen()
     screen.setup(width=screenWidth,height=screenHeight)
     return screen
-
+# 
 screenWidth=300
 screenHeight=500
 screen=setup_screen(screenWidth,screenHeight)
